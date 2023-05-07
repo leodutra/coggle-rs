d04966b1e54b9ead19aaa04dcc002de374000977
Leo Dutra <leodutra.br@gmail.com>
2023-05-07 00:03:12 -0300

    wip

--- src/api/http.rs
+++
@@ -1,6 +0,0 @@
-use reqwest::Client;
-
-lazy_static! {
-    // The Client holds a connection pool internally, so it is advised that you create one and reuse it.
-    pub static ref HTTP_CLIENT: Client = Client::new();
-}
